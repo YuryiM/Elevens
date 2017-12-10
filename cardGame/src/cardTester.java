e780caf32903102cb52663d81bff444e7514de6a
Yuryi M <ymironchyk@gmail.com>
2017-12-09 16:03:44 -0800

    Initial Commit

---
+++ cardGame/src/cardTester.java
@@ -0,0 +1,12 @@
+/**
+ * This is a class that tests the Card class.
+ */
+public class cardTester {
+
+    /**
+     * The main method in this class checks the Card operations for consistency.
+     *	@param args is not used.
+     */
+    public static void main(String[] args) {
+    }
+}
